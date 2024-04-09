bebfb9a1cd8f434139bbed2ffb79061e9b01305e
DuncanMoyo <duncanfmoyo@gmail.com>
2024-04-09 05:40:02 +0200

    made the profile section more mobile responsive

--- components/shared/Profile.tsx
+++ components/shared/Profile.tsx
@@ -1,7 +1,7 @@
 const Profile = () => {
   return (
-    <div className="bg-lightestTeal h-screen w-screen flex mx-auto max-w-7xl">
-      <div className="flex flex-1 m-20 bg-lighterTeal flex-col">
+    <div className="bg-lightestTeal h-screen w-screen mx-auto max-w-7xl flex flex-col md:flex-row">
+      <div className="flex flex-1 m-2 md:m-20 bg-lighterTeal flex-col order-2 md:order-1">
         <h1 className="uppercase text-3xl text-center text-white">
           About Dr Msutu
         </h1>
@@ -10,7 +10,7 @@ const Profile = () => {
           blend with the image provided
         </p>
       </div>
-      <div className="flex flex-1 bg-lightTeal justify-center text-white text-6xl m-2 text-center items-center">
+      <div className="flex flex-1 bg-lightTeal justify-center text-white text-6xl m-2 text-center items-center order-1 md:order-2">
         Image of Dr Sinovuyo
       </div>
     </div>
